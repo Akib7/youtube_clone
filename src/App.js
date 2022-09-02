355888c6fc4181a444c06962ce514b7bca4ebfb8
Akib Ahmed <akibahmed7360@gmail.com>
2022-09-02 19:34:41 +0600

    added routes and their components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import Navbar from "./components/navbar/Navbar";
+import Feed from "./components/feed/feed";
+import VideoDetails from "./components/video details/video-details";
+import ChannelDetails from "./components/channel-details/channel-details";
+import SearchFeed from "./components/search-feed/search-feed";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />}></Route>
+        <Route path="/video/:id" element={<VideoDetails />}></Route>
+        <Route path="/channel/:id" element={<ChannelDetails />}></Route>
+        <Route path="/search/:searchTerm" element={<SearchFeed />}></Route>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
