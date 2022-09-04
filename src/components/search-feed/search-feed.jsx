2f5944a67af9ee7bdf2d89553326f671526f4a11
Akib Ahmed <akibahmed7360@gmail.com>
2022-09-04 15:05:06 +0600

    successfully implemented the search feed in the project

--- src/components/search-feed/search-feed.jsx
+++ src/components/search-feed/search-feed.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import { Box, Typography } from "@mui/material";
+import { Videos } from "../";
+import { useParams } from "react-router-dom";
+
+import { fetchFromAPI } from "../../utils/fetchFromApi";
 
 const SearchFeed = () => {
-  return <div></div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams(); //useParam is helping to get the input we are writting in the searchBar
+  //useEffect is for when the app loads for the first time
+  //it's like initState()
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      // console.log(data);
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search for videos.{" "}
+        <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
